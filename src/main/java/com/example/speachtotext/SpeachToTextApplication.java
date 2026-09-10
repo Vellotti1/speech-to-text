4fd000699e8d3c849f7b7dc3f66911fca5d91e53
Vellotti1 <velja004@mymail.unisa.edu.au>
2026-09-10 21:30:04 +0930

    Initial Springboot

---
+++ src/main/java/com/example/speachtotext/SpeachToTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.speachtotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeachToTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeachToTextApplication.class, args);
+	}
+
+}
