7772651b2edc289b27ebf2759a43290cc7a1975d
Vellotti1 <velja004@mymail.unisa.edu.au>
2026-09-11 10:02:28 +0930

    Implimenting browser microphone

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,46 @@
-const recordBytton = document.getElementById("recordButton");
+const recordButton = document.getElementById("recordButton");
 const status = document.getElementById("status");
 
-recordButton.addEventListener("click", () => {
-    status.textContent = "Recording In Progress..."
+let mediaRecorder;
+let audioChunks =[];
+
+recordButton.addEventListener("click", async () => {
+    if (!mediaRecorder || mediaRecorder.state === "inactive") {
+        await startRecording();
+    } else {
+        stopRecording();
+    }
 });
+
+async function startRecording() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({audio:true});
+
+        audioChunks = [];
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+        mediaRecorder.addEventListener("stop", () => {
+            stream.getTracks().forEach(track => track.stop());
+        });
+
+        mediaRecorder.start();
+
+        recordButton.textContent = "End Recording";
+        status.textContent = "Now Recording...";   
+    } catch (error) {
+        status.textContent = "Please enable Microphone.";
+        console.error(error);
+    }
+
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+
+    recordButton.textContent = "Start Recording";
+    status.textContent = "Recording Ended";
+}
