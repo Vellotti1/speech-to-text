6fdf38bd95a7ec821c762d47f097b3a8a3f49c0f
Vellotti1 <velja004@mymail.unisa.edu.au>
2026-09-11 16:16:35 +0930

    Adding shutdown endpoint

--- src/main/java/com/example/speechtotext/controller/AdminController.java
+++ src/main/java/com/example/speechtotext/controller/AdminController.java
@@ -7,12 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/api/v1/admin")
 public class AdminController {
 
     private final Instant serverStart;
+    private boolean isShutingdown = false;
 
     public AdminController () {
         this.serverStart = Instant.now();
@@ -34,10 +38,30 @@ public class AdminController {
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/shutdown")
+    public ResponseEntity<ShutdownResponse> shutdown() {
+        if (isShutingdown) {
+            return ResponseEntity
+            .status(HttpStatus.CONFLICT)
+            .body(new ShutdownResponse("Shutdown in progress"));
+        }
+
+        isShutingdown = true;
+
+        return ResponseEntity
+        .status(HttpStatus.ACCEPTED)
+        .body(new ShutdownResponse("Shutdown Requested"));
+
+    }
+
     public record UptimeResponse(
         Instant utcServerStart,
         Instant utcNow,
         double serverUptimeSeconds
+    ) {}
+
+    public record ShutdownResponse(
+        String message
     ) {}
 
 
